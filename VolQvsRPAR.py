4f0ede4137082d7c5c44efc2a4952b8118a21c60
mismatchedcollar <106934390+mismatchedcollar@users.noreply.github.com>
2022-06-26 13:24:17 -0400

    Update VolQvsRPAR.py

--- VolQvsRPAR.py
+++ VolQvsRPAR.py
@@ -1,82 +1,32 @@
 import bt
 import matplotlib
 import matplotlib.pyplot as plt
-import matplotlib.ticker as mtick
 import pandas as pd
 from datetime import date, timedelta
-matplotlib.use('MacOSX')
+matplotlib.use('MacOSX') #Use 'TkAgg' if not on MacOSX 
+#%matplotlib inline 
 
-#### VOLQ PIG PORTFOLIO ####
-pig_shorts, pig_longs, pig_hedges, pig_cashpct, pig_hedgepct, pig_rebaldates, pig_positions  = [],[],[],[],[],[],[]
-
-pig_rebaldates.append('2021-11-09') ## 09 Nov 2021: Initial Pig Portfolio.
-pig_shorts.append({'FB','MSFT','REM', 'GOOGL','QQQ','AMZN','GSG','DIA','GDX','SPY','LQD','HYG','IGSB','TLT','AAPL','EMB','IWM','NVDA','VGK','EEM','VNQ','CPER','BITO'})
-pig_longs.append({'IEI','SHY','SHV','BIL','GLD','UUP','FXF','FXY','VXX'})
-pig_hedges.append({})
-pig_cashpct.append(0.0)
-pig_hedgepct.append(0.0)
-
-pig_rebaldates.append('2022-02-24') ## 24 Feb 2022: Risk Management Exited Commodity Shorts.
-pig_shorts.append({'TLT','EMB','IWM','DIA','SPY','QQQ','EEM','VGK','REM','VNQ','BITO','LQD','HYG','IGSB','AAPL','AMZN','FB','GOOGL','MSFT','NVDA'})
-pig_longs.append({'IEI','SHY','SHV','BIL','UUP','FXF','FXY','VXX','GLD'})
-pig_hedges.append({})
-pig_cashpct.append(0.0)
-pig_hedgepct.append(0.0)
-
-pig_rebaldates.append('2022-03-31') ## 31 Mar 2022: Quarterly Rebalance. Added Semiconductor, US & EU Financials, JNK Shorts. Added SLV Long. Went to 25% Cash.
-pig_shorts.append({'TLT','EMB','IWM','DIA','SPY','QQQ','EFA','EEM','VGK','REM','VNQ','SMH','JNK','IJH','BITO','LQD','HYG','IGSB','XLF','EUFN','AAPL','AMZN','FB','GOOGL','MSFT','NVDA'})
-pig_longs.append({'BIL','SHV','SHY','IEI','STIP','UUP','FXF','FXY','GLD','SLV'})
-pig_hedges.append({})
-pig_cashpct.append(0.25)
-pig_hedgepct.append(0.0)
-
-pig_rebaldates.append('2022-05-20') ## 20 May 2022: Hedged Pig. Added Dirty Beta Hedge, 5% ES=F, 5% RTY-F, 5% NQ=F. Went to 35% Cash.
-pig_shorts.append({'TLT','EMB','IWM','DIA','SPY','QQQ','EFA','EEM','VGK','REM','VNQ','SMH','JNK','IJH','BITO','LQD','HYG','IGSB','XLF','EUFN','AAPL','AMZN','FB','GOOGL','MSFT','NVDA'})
-pig_longs.append({'BIL','SHV','SHY','IEI','STIP','UUP','FXF','FXY','GLD','SLV'})
-pig_hedges.append({'ES=F','RTY=F','NQ=F'})
-pig_cashpct.append(0.35)
-pig_hedgepct.append(0.15)
-
-for i,v in enumerate(pig_rebaldates): ## Equal Weight Longs and Shorts based on Core % Invested (100%-Cash%-Hedge%). Equal Weight Hedge positions based on Hedge%.
-    s, l, h = {},{},{}
-    for key in pig_shorts[i]: s[key] = (1-(pig_cashpct[i]+pig_hedgepct[i]))/(len(pig_shorts[i])+len(pig_longs[i])) * -1 
-    for key in pig_longs[i]:  l[key] = (1-(pig_cashpct[i]+pig_hedgepct[i]))/(len(pig_shorts[i])+len(pig_longs[i]))
-    for key in pig_hedges[i]: h[key] = pig_hedgepct[i]/len(pig_hedges[i])
-    pig_positions.append(s|l|h)
-pig_weights = pd.DataFrame(pig_positions, index=pig_rebaldates)
-pig_weights = pig_weights.fillna(0.0)
-pig_weights.index = pd.to_datetime(pig_weights.index)
-pig_weights.index.name = "Date"
-
-pig_prices  = bt.get(list(pig_weights), clean_tickers=False, start='2021-11-09', end=date.today()-timedelta(days=1))
-pig_strategy = bt.Strategy('Pig Portfolio', 
- algos = [
- bt.algos.SelectAll(),
- bt.algos.WeighTarget(pig_weights),
- bt.algos.Rebalance(),
- ]
-)
-pig_bt = bt.Backtest(pig_strategy,pig_prices)
-#### END VOLQ PIG PORTFOLIO ####
-
-#### RPAR ETF ####
-rpar_prices = bt.get(['RPAR'], clean_tickers=False, start='2021-11-09', end=date.today()-timedelta(days=1))
-rpar_strategy = bt.Strategy('RPAR ETF', 
- algos = [  
- bt.algos.SelectAll(),
- bt.algos.WeighEqually(),
- bt.algos.Rebalance(),
- ]
-)
-rpar_bt = bt.Backtest(rpar_strategy,rpar_prices)
-#### END RPAR ETF ####
-
-#### SHOW REPORT ####
-report = bt.run(pig_bt,rpar_bt)
+pos = {} #Set Up Core Positions for Each Rebalance Date
+pos[pd.to_datetime('2021-11-09')] = dict.fromkeys(['TLT','EMB','IWM','DIA','SPY','QQQ','EEM','VGK','REM','VNQ','BITO','LQD','HYG','IGSB','AAPL','AMZN','FB','GOOGL','MSFT','NVDA','GDX','CPER','GSG'], -1.0) | dict.fromkeys(['IEI','SHY','SHV','BIL','GLD','UUP','FXF','FXY','VXX'], 1.0)
+pos[pd.to_datetime('2022-02-24')] = dict.fromkeys(['TLT','EMB','IWM','DIA','SPY','QQQ','EEM','VGK','REM','VNQ','BITO','LQD','HYG','IGSB','AAPL','AMZN','FB','GOOGL','MSFT','NVDA'], -1.0) | dict.fromkeys(['IEI','SHY','SHV','BIL','UUP','FXF','FXY','VXX','GLD'], 1.0)
+pos[pd.to_datetime('2022-03-31')] = dict.fromkeys(['TLT','EMB','IWM','DIA','SPY','QQQ','EFA','EEM','VGK','REM','VNQ','SMH','JNK','IJH','BITO','LQD','HYG','IGSB','XLF','EUFN','AAPL','AMZN','FB','GOOGL','MSFT','NVDA'], -0.75) | dict.fromkeys(['BIL','SHV','SHY','IEI','STIP','UUP','FXF','FXY','GLD','SLV'], 0.75)
+pos[pd.to_datetime('2022-05-20')] = dict.fromkeys(['TLT','EMB','IWM','DIA','SPY','QQQ','EFA','EEM','VGK','REM','VNQ','SMH','JNK','IJH','BITO','LQD','HYG','IGSB','XLF','EUFN','AAPL','AMZN','FB','GOOGL','MSFT','NVDA'], -0.50) | dict.fromkeys(['BIL','SHV','SHY','IEI','STIP','UUP','FXF','FXY','GLD','SLV'], 0.50)
+for k in pos: pos[k].update((x, y*1/len(pos[k])) for x, y in pos[k].items()) #Equal Weight Core Positions                                   
+pos[pd.to_datetime('2022-05-20')] = pos[pd.to_datetime('2022-05-20')] | dict.fromkeys(['ES=F','NQ=F','RTY=F'], 0.05) #Add Hedges Where Applicable
+pig = pd.DataFrame.from_dict(pos,orient='index').fillna(0.0) 
+pig.index.name = 'Date'
+pigprices = bt.get(list(pig), clean_tickers=False, start=pig.first_valid_index(), end=date.today())
+parpos = {} #Set up Risk Parity Positions for Each Rebalance Date
+parpos[pd.to_datetime('2021-11-09')] = {'RPAR':1.0}
+parpos[pd.to_datetime('2022-01-04')] = {'UPAR':1.0}
+par = pd.DataFrame.from_dict(parpos,orient='index').fillna(0.0) 
+par.index.name = 'Date'
+rparprices = bt.get(['RPAR'], clean_tickers=False, start=pig.first_valid_index(), end=date.today())
+uparprices = bt.get(['UPAR'], clean_tickers=False, start=pig.first_valid_index(), end=date.today())
+parprices = rparprices.merge(uparprices, left_index=True, right_index=True,how='outer').fillna(0.0)
+report = bt.run(bt.Backtest(data=pigprices, strategy=bt.Strategy('Pig ETF Proxy', algos=[bt.algos.SelectAll(), bt.algos.WeighTarget(pig), bt.algos.Rebalance()])),
+                bt.Backtest(data=parprices, strategy=bt.Strategy('RPAR->UPAR', algos=[bt.algos.SelectAll(), bt.algos.WeighTarget(par), bt.algos.Rebalance()])))
 report.display()
-report.display_monthly_returns()
 report.plot()
-plt.gca().yaxis.set_major_formatter(mtick.PercentFormatter(xmax=100.0))
-plt.title('Pig Portfolio (No Wifey 3x, Intras, Or Options) vs. Risk Parity ETF')
+plt.title('VolQ Pig Portfolio ETF Proxy\nPerformance Since Inception (9 Nov 2021)')
 plt.show()
-#### END SHOW REPORT ####
